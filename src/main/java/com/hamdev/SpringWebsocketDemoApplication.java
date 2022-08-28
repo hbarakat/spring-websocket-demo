c6093e9162c837d7e88758c0775ce46cd00a6766
Hamid Barakat <barakat.hamid@gmail.com>
2022-08-28 16:05:52 +0300

    First Commit

---
+++ src/main/java/com/hamdev/SpringWebsocketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hamdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketDemoApplication.class, args);
+	}
+
+}
